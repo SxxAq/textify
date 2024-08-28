f8f66d26073787c434eb09ed8b736be459235143
SxxAq <aqueel192@gmail.com>
2024-08-29 02:28:39 +0530

    homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,11 @@ const Header = () => {
   return (
     <header className="flex items-center justify-between px-4 py-2 md:px-8 mx-4 md:mx-20">
       <h1 className="text-xl md:text-2xl font-medium">
-        <span className="text-blue-400 font-bold">Text</span>ify
+        <span className="text-blue-500 font-bold">Text</span>ify
       </h1>
-      <button className="flex items-center Btn gap-2 px-4 py-2 rounded-md">
+      <button className="flex items-center Btn gap-2 px-4 py-2 rounded-lg">
         <p className="text-lg font-medium tracking-wide">New</p>
-        <i className="fa-solid fa-plus text-blue-400 text-lg md:text-xl ml-1"></i>
+        <i className="fa-solid fa-plus text-blue-500 text-lg md:text-xl ml-1"></i>
       </button>
     </header>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,35 @@
 const Home = () => {
   return (
-    <main className="pb-24 flex-1 text-center p-4 gap-3 sm:gap-4 md:gap-5 flex flex-col justify-center">
-      <h1 className="font-bold tracking-wide text-5xl md:text-7xl">
-        <span className="text-blue-400 font-bold">Text</span>ify
+    <main className="pb-24 flex-1 text-center p-4 gap-5 sm:gap-6 md:gap-7 flex flex-col justify-center bg-gradient-to-b from-blue-100 to-white">
+      <h1 className="font-extrabold tracking-wide text-5xl md:text-7xl text-gray-800">
+        <span className="text-blue-500">Text</span>ify
       </h1>
-      <h3 className="font-medium md:text-lg">
+      <h3 className="font-medium md:text-lg text-gray-700">
         Record
-        <span className="mx-2 text-blue-400"><i className="fa-solid fa-arrow-right"></i></span>
+        <span className="mx-2 text-blue-500">
+          <i className="fa-solid fa-arrow-right"></i>
+        </span>
         Transcribe
-        <span className="mx-2 text-blue-400"><i className="fa-solid fa-arrow-right"></i></span>
+        <span className="mx-2 text-blue-500">
+          <i className="fa-solid fa-arrow-right"></i>
+        </span>
         Translate
       </h3>
+      <button className="flex items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4 bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-blue-600 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-400 active:bg-blue-700 duration-200">
+        <p>Record</p>
+        <i className="fa-solid fa-microphone"></i>
+      </button>
+      <p className="text-base text-gray-600">
+        Or{" "}
+        <label className="text-blue-500 cursor-pointer hover:text-blue-600 duration-200">
+          upload
+          <input className="hidden" type="file" accept=".mp3,.wave" />
+        </label>{" "}
+        a mp3 file
+      </p>
+      <p className="mt-6 text-lg italic font-medium text-gray-800 bg-gradient-to-r from-blue-300 to-blue-400 text-transparent bg-clip-text">
+        Free Transcribe and Translate, No Strings Attached
+      </p>
     </main>
   );
 };
